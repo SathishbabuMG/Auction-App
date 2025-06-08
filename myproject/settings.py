dc2607aba6b445150d64f450e38cc8ab344edc0a
SathishbabuMG <sathishbabumg04@gmail.com>
2025-06-08 18:41:47 +0530

    k8s files added

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)7f+=0_4dbs&v$vn501)@%q%wd(9$brsfjitk^b@q$ios^%-lk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.8']
 
 
 # Application definition
